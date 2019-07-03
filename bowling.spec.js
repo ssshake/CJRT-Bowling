6f54b9195f564b709c91b354153c8c177b975461
Tuan N. Truong <t.truong@vehikl.com>
2019-07-03 14:48:47 -0400

    unexpected passing test

--- bowling.spec.js
+++ bowling.spec.js
@@ -30,7 +30,7 @@ describe('Bowling', () => {
       expect(bowling.score()).toEqual(16);
     });
 
-    xtest('consecutive spares each get a one roll bonus', () => {
+    test('consecutive spares each get a one roll bonus', () => {
       const rolls = [5, 5, 3, 7, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
       const bowling = new Bowling();
       rolls.forEach((roll) => { bowling.roll(roll); });
