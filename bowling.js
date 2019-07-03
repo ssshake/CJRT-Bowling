e086d8a8a9b868b47054bcd4a5d8dfd14da64c72
Tuan N. Truong <t.truong@vehikl.com>
2019-07-03 14:47:53 -0400

    refactor some shit

--- bowling.js
+++ bowling.js
@@ -1,39 +1,36 @@
 export class Bowling {
   constructor() {
     this.rolls = [];
-    this.frames = [];
   }
 
   roll(pins) {
     this.rolls = [...this.rolls, pins];
-
-   //let foo = Math.floor((this.rolls.length - 1) / 2) + 1
   }
 
   score() {
     return this.sumRolls();
   }
 
-  framesFromRolls() {
-
-    var i,j;
-    for (i=0,j=this.rolls.length; i<j; i+=2) {
-        this.frames.push(this.rolls.slice(i,i+2))
-    }
+  frames() {
+    return this.rolls.reduce((frames, roll, index, rolls) => {
+      if (index % 2){
+        return frames
+      }
+      return [...frames, [ roll, rolls[index + 1 ]]]
+    }, [])
   }
 
   sumRolls() {
-    this.framesFromRolls();
+    return this.frames().reduce((score, frame, index, frames) => score + this.getFrameScore(frame, index, frames), 0)
+  }
 
-    return this.frames.reduce((score, frame, index) => {
-      let frameScore = frame[0] + frame[1];
-      if (frameScore === 10) {
-        // add the next roll from the next frame
-        frameScore += this.frames[index + 1][0];
-      }
+  getFrameScore(frame, index, frames) {
+    let score = frame[0] + frame[1];
+    if (score === 10) {
+      score += frames[index + 1][0];
+    }
 
-      return score + frameScore
-    }, 0)
+    return score;
   }
 }
 
